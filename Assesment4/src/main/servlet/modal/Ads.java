31e4c2a7b8171abc0267259b3cdd90e7c88ea647
Maruvada <smaruvada@dxc.com>
2019-11-04 10:58:13 +0530

    first commit

---
+++ Assesment4/src/main/servlet/modal/Ads.java
@@ -0,0 +1,23 @@
+package main.servlet.modal;
+
+public class Ads {
+private String Category;
+
+public String getCategory() {
+	return Category;
+}
+
+public void setCategory(String category) {
+	Category = category;
+}
+
+
+
+public Ads(String category) {
+	super();
+	Category = category;
+
+}
+
+
+}
